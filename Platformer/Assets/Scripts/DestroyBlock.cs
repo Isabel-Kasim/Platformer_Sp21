65e3e6c96afff03775c8881c7c5454384d4b7106
Isabel_Kasim <54857733+Isabel-Kasim@users.noreply.github.com>
2021-02-21 23:14:01 -0800

    Platformer (pt 1)

---
+++ Platformer/Assets/Scripts/DestroyBlock.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class DestroyBlock : MonoBehaviour
+{
+
+
+    public GameObject Block;
+    public TextMeshProUGUI text;
+    public int points = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Block.GetComponent<GameObject>();
+    }
+
+    private void Update()
+    {
+        if(Input.GetMouseButton(0))
+        {
+            RaycastHit hit;
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+
+            if (Physics.Raycast(ray, out hit))
+            {
+                BoxCollider bc = hit.collider as BoxCollider;
+
+                if (bc != null)
+                {
+                    Destroy(bc.gameObject);
+
+                    if (bc.gameObject.tag == "Question Block")
+                    {
+                        points++;
+                        text.SetText(points.ToString());
+                    }
+                    
+                }
+            }
+        }
+    }
+
+    
+
+}
